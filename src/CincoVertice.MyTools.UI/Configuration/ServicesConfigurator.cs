e2b2f0ebb7076287c5ac7e0cb5ce7cf643262102
Ricardo Gonzalez Garza <rickygzz@hotmail.com>
2025-04-28 03:14:44 -0600

    Added dependency injection.

---
+++ src/CincoVertice.MyTools.UI/Configuration/ServicesConfigurator.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CincoVertice.MyTools.UI.Configuration;
+
+public static class ServicesConfigurator
+{
+    public static void Configure(IServiceCollection services)
+    {
+        services.AddTransient<MainForm>();
+    }
+}
--- src/CincoVertice.MyTools.UI/Program.cs
+++ src/CincoVertice.MyTools.UI/Program.cs
@@ -1,9 +1,13 @@
 using CincoVertice.Common.WinApi.Helpers;
+using CincoVertice.MyTools.UI.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CincoVertice.MyTools.UI;
 
 internal static class Program
 {
+    public static IServiceProvider? ServiceProvider { get; private set; }
+
     /// <summary>
     ///     The main entry point for the application.
     /// </summary>
@@ -17,9 +21,13 @@ internal static class Program
             return;
         }
 
+        var serviceCollection = new ServiceCollection();
+        ServicesConfigurator.Configure(serviceCollection);
+        ServiceProvider = serviceCollection.BuildServiceProvider();
+
         // To customize application configuration such as set high DPI settings or default font,
         // see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
-        Application.Run(new MainForm());
+        Application.Run(ServiceProvider.GetRequiredService<MainForm>());
     }
 }
