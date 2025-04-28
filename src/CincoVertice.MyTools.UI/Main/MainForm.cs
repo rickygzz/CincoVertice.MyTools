cd8fa7e1e80ed80e922b9dfd6d617ab37aba48d9
Ricardo Gonzalez Garza <rickygzz@hotmail.com>
2025-04-28 02:39:16 -0600

    Added common submodule and reused its .editorconfig file.

--- src/CincoVertice.MyTools.UI/Main/MainForm.cs
+++ src/CincoVertice.MyTools.UI/Main/MainForm.cs
@@ -1,16 +1,15 @@
-namespace CincoVertice.MyTools
-{
-    public partial class MainForm : Form
-    {
-        public MainForm()
-        {
-            InitializeComponent();
-        }
+namespace CincoVertice.MyTools;
 
-        private void ExitNotifyContextMenu_Click(object sender, EventArgs e)
-        {
-            Dispose();
-            Application.Exit();
-        }
+public partial class MainForm : Form
+{
+    public MainForm()
+    {
+        InitializeComponent();
+    }
+
+    private void ExitNotifyContextMenu_Click(object sender, EventArgs e)
+    {
+        Dispose();
+        Application.Exit();
     }
 }
